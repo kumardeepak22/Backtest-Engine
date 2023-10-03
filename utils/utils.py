ae4544e42756007c42e4214bfe274e454a17f923
Deepak <you@example.com>
2023-10-03 14:22:58 +0530

    add get_instrument_lot_size, generate strangle_strikes_and_symbols functions to utils

--- utils/utils.py
+++ utils/utils.py
@@ -14,8 +14,10 @@ BNF_WEEKLY_EXP_START_DATE = date(2016, 6, 27)
 NF_WEEKLY_EXP_START_DATE = date(2019, 2, 11)
 BANKNIFTY_SYMBOL = "BANKNIFTY"
 NIFTY_SYMBOL = "NIFTY"
+FINNIFTY_SYMBOL= "FINNIFTY"
 BANKNIFTY_LOT_SIZE = 25
 NIFTY_LOT_SIZE = 50
+FINNIFTY_LOT_SIZE = 40
 BANKNIFTY_MARGIN_REQUIRED = 180000
 NIFTY_MARGIN_REQUIRED = 130000
 NOTIONAL_VALUE_ASSUMED = 1000000
@@ -62,3 +64,45 @@ def get_opt_symbol(instrument: str, expiry_comp: str, strike: int, opt_type: str
     """
     opt_symbol = f"{instrument.upper()}{expiry_comp}{strike}{opt_type}"
     return opt_symbol
+
+def get_instrument_lot_size(instrument: str) -> int:
+    #Returns instrument lot size
+    if instrument == "BANKNIFTY":
+        return BANKNIFTY_LOT_SIZE
+    elif instrument == "FINNIFTY":
+        return FINNIFTY_LOT_SIZE
+    else:
+        return NIFTY_LOT_SIZE
+def generate_strangle_strikes_and_symbols(instrument: str, atm_strike: int, how_far_otm_short_point: int,
+                                             how_far_otm_hedge_point: int, expiry_comp: str) -> dict[str, typing.Any]:
+    """
+    Generates strikes, option symbols for strangle strategy
+    Args:
+        instrument(str): instrument name
+        atm_strike(int): at the money strike
+        how_far_otm_short_point(int): how far from at the money to short
+        how_far_otm_hedge_point(int): how far from at the money to buy hedge
+        expiry_comp(str): five letter expiry keyword   Returns:
+        dict[str, typing.Any]: returns dict of option symbols, strikes and list of option symbols
+    """
+    strangle_dict = {}
+    strangle_dict["ce_short_strike"] = atm_strike + how_far_otm_short_point
+    strangle_dict["pe_short_strike"] = atm_strike - how_far_otm_short_point
+    strangle_dict["ce_hedge_strike"] = atm_strike + how_far_otm_hedge_point
+    strangle_dict["pe_hedge_strike"] = atm_strike - how_far_otm_hedge_point
+    strangle_dict["ce_short_opt_symbol"] = get_opt_symbol(instrument, expiry_comp,
+                                                             strangle_dict["ce_short_strike"], "CE")
+    strangle_dict["pe_short_opt_symbol"] = get_opt_symbol(instrument, expiry_comp,
+                                                             strangle_dict["pe_short_strike"], "PE")
+    strangle_dict["ce_hedge_opt_symbol"] = get_opt_symbol(instrument, expiry_comp,
+                                                             strangle_dict["ce_hedge_strike"], "CE")
+    strangle_dict["pe_hedge_opt_symbol"] = get_opt_symbol(instrument, expiry_comp,
+                                                             strangle_dict["pe_hedge_strike"], "PE")
+    strangle_dict["trading_symbols"] = [strangle_dict["ce_short_opt_symbol"],
+                                           strangle_dict["pe_short_opt_symbol"],
+                                           strangle_dict["ce_hedge_opt_symbol"],
+                                           strangle_dict["pe_hedge_opt_symbol"]]
+    return strangle_dict
+
+
+
